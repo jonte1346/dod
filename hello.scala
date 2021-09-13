9197c8fb7e42e36ce8e76606833e047b8e50fa97
jonte1346 <jonathan.giegold@hotmail.se>
2021-09-13 19:04:18 +0200

    Lade till ett hejsan hoppsan

--- hello.scala
+++ hello.scala
@@ -1,5 +1,6 @@
 object HelloWorld {
-def main(args: Array[String]): Unit = {
-println("Hello, world! Its a me, Mario!")
-}
+    def main(args: Array[String]): Unit = {
+    println("Hello, world! Its a me, Mario!")
+    println("hejsan hoppsan!")
+    }
 }
