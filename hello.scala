89a47f38319356419013366481cd89d728c3787e
jonte1346 <jonathan.giegold@hotmail.se>
2021-09-13 19:12:50 +0200

    Adding Wario

--- hello.scala
+++ hello.scala
@@ -1,6 +1,6 @@
 object HelloWorld {
     def main(args: Array[String]): Unit = {
-    println("Hello, world! Its a me, Mario!")
+    println("Hello, world! Its a me, Mario!, and Wario!")
     println("hejsan hoppsan!")
     }
 }
