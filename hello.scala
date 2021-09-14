030ec81c79033a20f823fb929d099b14e8d9efb7
jonte1346 <jonathan.giegold@hotmail.se>
2021-09-14 10:31:09 +0200

    lägger till "där"

--- hello.scala
+++ hello.scala
@@ -1,6 +1,6 @@
 object HelloWorld {
     def main(args: Array[String]): Unit = {
     println("Hello, world! Its a me, Mario!, and Luigi")
-    println("hejsan hoppsan! det är jag, vad gör du")
+    println("hejsan hoppsan! det är jag, vad gör du där")
     }
 }
